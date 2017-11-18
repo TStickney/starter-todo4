7a0cfa2d55b8db0ad5d5f05be8ca4a8895e012d4
TStickney <tstick56@hotmail.com>
2017-11-17 18:40:23 -0800

    Added an XML model to pull data from the tasks.xml file.

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Tasks extends CSV_Model {            
+class Tasks extends XML_Model {            
 
     // Constructor
     public function __construct()
     {
-           parent::__construct(DATAPATH . 'tasks.csv', 'id');
+           parent::__construct(DATAPATH . 'tasks.xml', 'id');
            
     }
     
